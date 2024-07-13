6b4a1740dd7bbc58e47a38c3a588da681874ab9c
Jeremy Bahadirli <jeremybahadirli@gmail.com>
2024-07-12 19:53:25 -0500

    Add keybind to toggle diganostic virtual text

--- init.lua
+++ init.lua
@@ -166,6 +166,11 @@ vim.keymap.set('n', '[d', vim.diagnostic.goto_prev, { desc = 'Go to previous [D]
 vim.keymap.set('n', ']d', vim.diagnostic.goto_next, { desc = 'Go to next [D]iagnostic message' })
 vim.keymap.set('n', '<leader>e', vim.diagnostic.open_float, { desc = 'Show diagnostic [E]rror messages' })
 vim.keymap.set('n', '<leader>q', vim.diagnostic.setloclist, { desc = 'Open diagnostic [Q]uickfix list' })
+vim.keymap.set('n', '<leader>l', function()
+  local current_config = vim.diagnostic.config().virtual_text
+  local new_config = not current_config
+  vim.diagnostic.config { virtual_text = new_config }
+end, { desc = 'Toggle diagnostic virtua[l] text' })
 
 -- Exit terminal mode in the builtin terminal with a shortcut that is a bit easier
 -- for people to discover. Otherwise, you normally need to press <C-\><C-n>, which
